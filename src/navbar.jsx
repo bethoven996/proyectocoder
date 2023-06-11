6017538d84856222095757318ecc471ddefee083
bethoven996 <134743082+bethoven996@users.noreply.github.com>
2023-06-11 18:40:27 -0300

    proyecto para subir

---
+++ src/navbar.jsx
@@ -0,0 +1,12 @@
+import App from "./App";
+
+const navbar = () => {
+  return (
+    <header>
+      <ul>Nombre</ul>
+      <ul>Apellido</ul>
+      <ul>Contacto</ul>
+    </header>
+  );
+};
+export default App;
